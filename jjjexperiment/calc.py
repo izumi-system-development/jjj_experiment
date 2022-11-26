8bfe0519c80cd4c9f4e3e3f4cd8cfef7da801d57
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2022-11-26 15:06:50 +0900

    Update calc.py

--- jjjexperiment/calc.py
+++ jjjexperiment/calc.py
@@ -524,11 +524,11 @@ def get_E_E_C_d_t(Theta_hs_out_d_t, Theta_hs_in_d_t, Theta_ex_d_t, X_hs_out_d_t,
         # (2)
         E_E_C_d_t = E_E_comp_C_d_t + E_E_fan_C_d_t
     elif type == 'ルームエアコンディショナ活用型全館空調システム':
-        E_E_CRAC_C_d_t = rac2.calc_E_E_C_d_t(region, q_rtd_C, q_max_C, e_rtd_C, dualcompressor_C, q_hs_CS_d_t * 3.6 / 1000, q_hs_CL_d_t * 3.6 / 1000, input_C_af_C)
+        E_E_CRAC_C_d_t = rac2.calc_E_E_C_d_t(region, q_rtd_C, q_max_C, e_rtd_C, dualcompressor_C, q_hs_CS_d_t * 3.6 / 1000, q_hs_CL_d_t * 3.6 / 1000, input_C_af_C, outdoorFile)
 
         # (38)
         #E_E_fan_C_d_t = dc_a.get_E_E_fan_C_d_t(P_rac_fan_rtd_C, V_hs_vent_d_t, V_hs_supply_d_t, V_hs_dsgn_C, q_hs_CS_d_t * 3.6 / 1000 + q_hs_CL_d_t * 3.6 / 1000, f_SFP_C)
-        E_E_fan_C_d_t = dc_a2.get_E_E_fan_C_d_t(P_rac_fan_rtd_C, V_hs_vent_d_t, V_hs_supply_d_t, V_hs_dsgn_C, q_hs_CS_d_t * 3.6 / 1000 + q_hs_CL_d_t * 3.6 / 1000, f_SFP_C, outdoorFile)
+        E_E_fan_C_d_t = dc_a2.get_E_E_fan_C_d_t(P_rac_fan_rtd_C, V_hs_vent_d_t, V_hs_supply_d_t, V_hs_dsgn_C, q_hs_CS_d_t * 3.6 / 1000 + q_hs_CL_d_t * 3.6 / 1000, f_SFP_C)
 
         # (2)
         E_E_C_d_t = E_E_CRAC_C_d_t + E_E_fan_C_d_t
