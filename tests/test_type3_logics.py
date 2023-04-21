15d44a842bf10323dfcc34b430bacac1bcfeee90
izumi-t-yamana <t-yamana@izumi-system.co.jp>
2023-04-21 16:47:28 +0900

    refac: テストのマジックナンバー記述を最小化

--- tests/test_type3_logics.py
+++ tests/test_type3_logics.py
@@ -6,60 +6,73 @@ import jjjexperiment.constants as consts
 
 class Testコイル特性:
 
-    def test_有効面積_TYPE1_低定格(self):
-        """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_1, 5500)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_1, 5500)
-    def test_有効面積_TYPE1_閾定格(self):
-        """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_1, 5600)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_1, 5600)
-    def test_有効面積_TYPE1_高定格(self):
-        """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_1, 5700)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_1, 5700)
+    expected_T1T2 = {
+        'A_f_hex': 0.23559,
+        'A_e_hex': 6.396,
+    }
+    expected_T3 = {
+        'A_f_hex_upper': 0.3,
+        'A_f_hex_lower': 0.2,
+        'A_f_hex_custom': 0.23456,  # ユーザーの独自入力を想定
+        'A_e_hex_upper': 10.6,
+        'A_e_hex_lower': 6.2,
+        'A_e_hex_custom': 6.66666,  # ユーザーの独自入力を想定
+    }
 
-    def test_有効面積_TYPE2_低定格(self):
+    def test_有効面積_方式1_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_2, 5500)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_2, 5500)
-    def test_有効面積_TYPE2_閾定格(self):
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5500)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5500)
+    def test_有効面積_方式1_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_2, 5600)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_2, 5600)
-    def test_有効面積_TYPE2_高定格(self):
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5600)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5600)
+    def test_有効面積_方式1_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert 0.23559 == get_A_f_hex(PROCESS_TYPE_2, 5700)
-        assert 6.396   == get_A_e_hex(PROCESS_TYPE_2, 5700)
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5700)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5700)
 
-    def test_有効面積_TYPE3_低定格(self):
+    def test_有効面積_方式2_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert 0.2 == get_A_f_hex(PROCESS_TYPE_3, 5500)
-        assert 6.2 == get_A_e_hex(PROCESS_TYPE_3, 5500)
-    def test_有効面積_TYPE3_閾定格(self):
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5500)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5500)
+    def test_有効面積_方式2_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert 0.3 == get_A_f_hex(PROCESS_TYPE_3, 5600)
-        assert 10.6 == get_A_e_hex(PROCESS_TYPE_3, 5600)
-    def test_有効面積_TYPE3_高定格(self):
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5600)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5600)
+    def test_有効面積_方式2_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert 0.3 == get_A_f_hex(PROCESS_TYPE_3, 5700)
-        assert 10.6 == get_A_e_hex(PROCESS_TYPE_3, 5700)
+        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5700)
+        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5700)
 
-    def test_有効面積_TYPE3_低定格_上書き(self):
+    def test_有効面積_方式3_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        consts.A_f_hex_small_H = 0.23456  # ユーザー入力時に上書きした場合
-        consts.A_e_hex_small_H = 6.66666  # ユーザー入力時に上書きした場合
-        assert 0.23456 == get_A_f_hex(PROCESS_TYPE_3, 5500)
-        assert 6.66666 == get_A_e_hex(PROCESS_TYPE_3, 5500)
-    def test_有効面積_TYPE3_閾定格_上書き(self):
+        assert self.expected_T3['A_f_hex_lower'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
+        assert self.expected_T3['A_e_hex_lower'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
+    def test_有効面積_方式3_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        consts.A_f_hex_large_H = 0.34567  # ユーザー入力時に上書きした場合
-        consts.A_e_hex_large_H = 9.99999  # ユーザー入力時に上書きした場合
-        assert 0.34567 == get_A_f_hex(PROCESS_TYPE_3, 5600)
-        assert 9.99999 == get_A_e_hex(PROCESS_TYPE_3, 5600)
-    def test_有効面積_TYPE3_高定格_上書き(self):
+        assert self.expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
+        assert self.expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
+    def test_有効面積_方式3_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        consts.A_f_hex_large_H = 0.34567  # ユーザー入力時に上書きした場合
-        consts.A_e_hex_large_H = 9.99999  # ユーザー入力時に上書きした場合
-        assert 0.34567 == get_A_f_hex(PROCESS_TYPE_3, 5700)
-        assert 9.99999 == get_A_e_hex(PROCESS_TYPE_3, 5700)
+        assert self.expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
+        assert self.expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
+
+    def test_有効面積_方式3_定格低_上書き(self):
+        """ 定格能力 5.6 kW 未満で 仕様書通り """
+        consts.A_f_hex_small_H = self.expected_T3['A_f_hex_custom']
+        consts.A_e_hex_small_H = self.expected_T3['A_e_hex_custom']
+        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
+        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
+    def test_有効面積_方式3_定格BVA_上書き(self):
+        """ 定格能力 5.6 kW 丁度で 仕様書通り """
+        consts.A_f_hex_large_H = self.expected_T3['A_f_hex_custom']
+        consts.A_e_hex_large_H = self.expected_T3['A_e_hex_custom']
+        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
+        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
+    def test_有効面積_方式3_定格高_上書き(self):
+        """ 定格能力 5.6 kW 以上で 仕様書通り """
+        consts.A_f_hex_large_H = self.expected_T3['A_f_hex_custom']
+        consts.A_e_hex_large_H = self.expected_T3['A_e_hex_custom']
+        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
+        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
