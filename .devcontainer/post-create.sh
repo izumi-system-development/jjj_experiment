ad67f2163e9c6f5248509fc25fc49bc816e5f832
izumi-t-yamana <t-yamana@izumi-system.co.jp>
2023-05-09 05:29:28 +0000

    pytest 導入用

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,9 @@
 # pip の更新
 pip3 install --user -U pip
 
+# numpy, pandas などは Pyhees 使用バージョンが自動的に追加されるため不要
+pip3 install --user -r requirements.txt
+
 # Pyhees レポジトリは Package リリースがされていないため requirements.txt に含めることができない
 pip3 install --user git+https://github.com/BRI-EES-House/pyhees.git
 
