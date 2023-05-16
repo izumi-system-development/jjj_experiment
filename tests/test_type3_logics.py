03935dc9f6dcbea93ce51aa4a66b44881548c3c9
izumi-t-yamana <t-yamana@izumi-system.co.jp>
2023-05-16 06:24:26 +0000

    debug: constを更新するテストはデフォルトに戻す処理を追加

--- tests/test_type3_logics.py
+++ tests/test_type3_logics.py
@@ -20,11 +20,12 @@ INPUT_PATH = './tests/inputs/default_testinput.json'
 
 class Testコイル特性:
 
-    expected_T1T2 = {
+    _expected_T1T2 = {
         'A_f_hex': 0.23559,  # 仕様書より
         'A_e_hex': 6.396,    # 仕様書より
     }
-    expected_T3 = {
+
+    _expected_T3 = {
         'A_f_hex_upper': 0.3,  # 仕様書より
         'A_f_hex_lower': 0.2,  # 仕様書より
         'A_f_hex_custom': 0.23456,  # ユーザーの独自入力を想定
@@ -35,61 +36,85 @@ class Testコイル特性:
 
     def test_有効面積_方式1_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5500)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5500)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5500)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5500)
     def test_有効面積_方式1_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5600)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5600)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5600)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5600)
     def test_有効面積_方式1_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5700)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5700)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_1, 5700)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_1, 5700)
 
     def test_有効面積_方式2_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5500)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5500)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5500)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5500)
     def test_有効面積_方式2_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5600)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5600)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5600)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5600)
     def test_有効面積_方式2_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert self.expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5700)
-        assert self.expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5700)
+        assert self._expected_T1T2['A_f_hex'] == get_A_f_hex(PROCESS_TYPE_2, 5700)
+        assert self._expected_T1T2['A_e_hex'] == get_A_e_hex(PROCESS_TYPE_2, 5700)
 
     def test_有効面積_方式3_定格低(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        assert self.expected_T3['A_f_hex_lower'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
-        assert self.expected_T3['A_e_hex_lower'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
+        assert self._expected_T3['A_f_hex_lower'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
+        assert self._expected_T3['A_e_hex_lower'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
     def test_有効面積_方式3_定格BVA(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        assert self.expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
-        assert self.expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
+        assert self._expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
+        assert self._expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
     def test_有効面積_方式3_定格高(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        assert self.expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
-        assert self.expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
+        assert self._expected_T3['A_f_hex_upper'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
+        assert self._expected_T3['A_e_hex_upper'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
+
+
+class Testコイル特性_方式3_ユーザー独自値:
+    """ 潜熱評価モデル時のコイル特性の係数はユーザーの独自値が設定可能としています
+    """
+    _custom_values = {
+        'H_A': {
+            'A_f_hex_small': 0.12345,  # ユーザーの独自入力を想定
+            'A_e_hex_small': 6.66666,  # ユーザーの独自入力を想定
+            'A_f_hex_large': 0.23456,  # ユーザーの独自入力を想定
+            'A_e_hex_large': 7.77777,  # ユーザーの独自入力を想定
+        }
+    }
+    _default_values = {
+        'H_A': {
+            'A_f_hex_small': 0.2,   # 規定値
+            'A_e_hex_small': 6.2,   # 規定値
+            'A_f_hex_large': 0.3,   # 規定値
+            'A_e_hex_large': 10.6,  # 規定値
+        }
+    }
+    @classmethod
+    def setup_class(cls):
+        """ ユーザーカスタム値を適用する """
+        consts.set_constants(cls._custom_values)
+
+    @classmethod
+    def teardown_class(cls):
+        """ ユーザーカスタム値をデフォルト値に戻す """
+        consts.set_constants(cls._default_values)
 
     def test_有効面積_方式3_定格低_上書き(self):
         """ 定格能力 5.6 kW 未満で 仕様書通り """
-        consts.A_f_hex_small_H = self.expected_T3['A_f_hex_custom']
-        consts.A_e_hex_small_H = self.expected_T3['A_e_hex_custom']
-        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
-        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
+        assert self._custom_values['H_A']['A_f_hex_small'] == get_A_f_hex(PROCESS_TYPE_3, 5500)
+        assert self._custom_values['H_A']['A_e_hex_small'] == get_A_e_hex(PROCESS_TYPE_3, 5500)
     def test_有効面積_方式3_定格BVA_上書き(self):
         """ 定格能力 5.6 kW 丁度で 仕様書通り """
-        consts.A_f_hex_large_H = self.expected_T3['A_f_hex_custom']
-        consts.A_e_hex_large_H = self.expected_T3['A_e_hex_custom']
-        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
-        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
+        assert self._custom_values['H_A']['A_f_hex_large'] == get_A_f_hex(PROCESS_TYPE_3, 5600)
+        assert self._custom_values['H_A']['A_e_hex_large'] == get_A_e_hex(PROCESS_TYPE_3, 5600)
     def test_有効面積_方式3_定格高_上書き(self):
         """ 定格能力 5.6 kW 以上で 仕様書通り """
-        consts.A_f_hex_large_H = self.expected_T3['A_f_hex_custom']
-        consts.A_e_hex_large_H = self.expected_T3['A_e_hex_custom']
-        assert self.expected_T3['A_f_hex_custom'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
-        assert self.expected_T3['A_e_hex_custom'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
+        assert self._custom_values['H_A']['A_f_hex_large'] == get_A_f_hex(PROCESS_TYPE_3, 5700)
+        assert self._custom_values['H_A']['A_e_hex_large'] == get_A_e_hex(PROCESS_TYPE_3, 5700)
 
 class Test熱伝達特性_冷房:
 
