85fa2c65afd2dccb895db930edab6e9b554c986e
izumi-t-yamana <t-yamana@izumi-system.co.jp>
2023-08-24 17:28:50 +0900

    fix: デフォルト入力は潜熱バグ修正しない

--- tests/test_integration_execution.py
+++ tests/test_integration_execution.py
@@ -12,10 +12,19 @@ from jjjexperiment.logger import LimitedLoggerAdapter as _logger
 
 class Test既存計算維持_デフォルト入力時:
 
+    # WARNING: 現状プラットフォームのデフォルト入力が 潜熱バグFix=OFF となっている
+    # 正しい結果が必要なら fix_latent_load = 2 に上書きする
     _inputs1: dict = json.load(open(INPUT_SAMPLE_TYPE1_PATH, 'r'))
     _inputs2: dict = json.load(open(INPUT_SAMPLE_TYPE2_PATH, 'r'))
     _inputs3: dict = json.load(open(INPUT_SAMPLE_TYPE3_PATH, 'r'))
     _inputs4: dict = json.load(open(INPUT_SAMPLE_TYPE4_PATH, 'r'))
+
+    def fix_latent_bug(self, inputs: dict) -> dict:
+        """ 潜熱バグ修正時の計算結果を確認したい時に使用する
+        """
+        inputs.update({"fix_latent_load": "2"})
+        # NOTE: もしも calc に通さないなら追加で set_constants() が必要
+        return inputs
 
     def test_インプットデータ_前提確認(self, expected_inputs):
         """ テストコードが想定しているインプットデータかどうか確認
@@ -28,6 +37,12 @@ class Test既存計算維持_デフォルト入力時:
         assert result['TInput'].q_max_H == expected_inputs.q_max_H
         assert result['TInput'].e_rtd_C == expected_inputs.e_rtd_C
         assert result['TInput'].e_rtd_H == expected_inputs.e_rtd_H
+
+    def test_計算実行_バリデーションしない(self):
+        """
+        """
+        _logger.init_logger()
+        result = calc(self.fix_latent_bug(self._inputs1), test_mode=True)
 
     def test_計算結果一致_方式1(self, expected_result_type1):
         """ ipynbのサンプル入力で計算結果が意図しない変化がないことを確認
