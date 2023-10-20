faa4b75f9e99962572d1c1d6c11382ab1c604a84
izumi-t-yamana <t-yamana@izumi-system.co.jp>
2023-10-20 04:44:09 +0000

    update version

--- jjjexperiment/calc.py
+++ jjjexperiment/calc.py
@@ -27,7 +27,7 @@ def version_info() -> str:
     """
     # NOTE: subprocessモジュールによるコミット履歴からの生成は \
     # ipynb 環境では正常に動作しませんでした(returned no-zero exit status 128.)
-    return '_20231011'
+    return '_20231020'
 
 def calc_Q_UT_A(case_name, A_A, A_MR, A_OR, A_env, mu_H, mu_C, q_hs_rtd_H, q_hs_rtd_C, q_rtd_H, q_rtd_C, q_max_H, q_max_C, V_hs_dsgn_H, V_hs_dsgn_C, Q,
             VAV, general_ventilation, hs_CAV, duct_insulation, region, L_H_d_t_i, L_CS_d_t_i, L_CL_d_t_i,
@@ -809,6 +809,11 @@ def calc_E_E_H_d_t(
             # NOTE: 求めたいのは循環ファンなので P_rac_fan ではなく P_fan を使用する
             E_E_fan_H_d_t = dc_a.get_E_E_fan_H_d_t(type, P_fan_rtd_H, V_hs_vent_d_t, V_hs_supply_d_t, V_hs_dsgn_H, q_hs_H_d_t, f_SFP_H)
 
+            _logger.NDdebug("V_hs_vent_d_t", V_hs_vent_d_t)
+            _logger.NDdebug("V_hs_supply_d_t", V_hs_supply_d_t)
+            _logger.NDdebug("q_hs_H_d_t", q_hs_H_d_t)
+            _logger.NDdebug("E_E_fan_H_d_t", E_E_fan_H_d_t)
+
             df_output_denchuH = pd.DataFrame(index = pd.date_range(
                 datetime(2023,1,1,1,0,0), datetime(2024,1,1,0,0,0), freq='h'))
 
@@ -962,6 +967,11 @@ def calc_E_E_C_d_t(
                 E_E_C_d_t = E_E_CRAC_C_d_t + E_E_fan_C_d_t  # kW
             )
             df_output_denchuC.to_csv(case_name + version_info() + '_denchu_C_output.csv', encoding='cp932')  # =Shift_JIS
+
+            _logger.NDdebug("V_hs_vent_d_t", V_hs_vent_d_t)
+            _logger.NDdebug("V_hs_supply_d_t", V_hs_supply_d_t)
+            _logger.NDdebug("q_hs_H_d_t", q_hs_C_d_t)
+            _logger.NDdebug("E_E_fan_H_d_t", E_E_fan_C_d_t)
 
         _logger.NDdebug("E_E_CRAC_C_d_t", E_E_CRAC_C_d_t)
         _logger.NDdebug("E_E_fan_C_d_t", E_E_fan_C_d_t)
