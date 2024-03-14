ae93e9654b4ab36906ea7c94630069c88897a765
izumi-t-yamana <t-yamana@izmc.co.jp>
2024-03-14 04:31:56 +0000

    feat: 委員提案キャップロジック2の追加

--- jjjexperiment/jjj_section4_2.py
+++ jjjexperiment/jjj_section4_2.py
@@ -1876,36 +1876,113 @@ def cap_V_supply_d_t_i(V_supply_d_t_i, V_dash_supply_d_t_i, V_vent_g_i, region, 
         new_V_supply_d_t_i = np.clip(V_supply_d_t_i, V_vent_g_i, V_dash_supply_d_t_i)
 
     elif constants.change_V_supply_d_t_i_max == Vサプライの上限キャップ.全体でキャップ.value:
-        V_supply_d_t_i = np.clip(V_supply_d_t_i, V_vent_g_i, None)
-        V_supply_d_t = np.sum(V_supply_d_t_i, axis=0)  # 1d-shape(5, )
+        # デンソーエアクール 守屋さまより提案 案1
 
-        _logger.NDdebug("V_supply_d_t_i_キャップ後:", V_supply_d_t_i[0])
-
-        # キャップとしての設計風量
+        """ 設計風量をキャップ上限とする """
         V_hs_dsgn_C = V_hs_dsgn_C if V_hs_dsgn_C is not None else float('inf')
         V_hs_dsgn_H = V_hs_dsgn_H if V_hs_dsgn_H is not None else float('inf')
 
-        overflow_mask_H = np.logical_and(H, V_supply_d_t > V_hs_dsgn_H)
-        overflow_mask_C = np.logical_and(C, V_supply_d_t > V_hs_dsgn_C)
+        """ キャップを超える時刻を調べる """
+        V_supply_d_t_i = np.clip(V_supply_d_t_i, V_vent_g_i, None)
+        V_supply_d_t = np.sum(V_supply_d_t_i, axis=0)  # 1d-shape(5, )
 
+        overflow_mask_H_d_t = np.logical_and(H, V_supply_d_t > V_hs_dsgn_H)
+        overflow_mask_C_d_t = np.logical_and(C, V_supply_d_t > V_hs_dsgn_C)
+
+        """ 全体にかける縮小率を算出 """  # 全体適用なので案1では1d-array
         ratios_H = np.divide(
           np.full(len(V_supply_d_t), V_hs_dsgn_H, dtype=float),
           np.ceil(V_supply_d_t * 1000) / 1000,  # NOTE: 計算速度と四捨五入による設計風量超え防止のため
-          where=overflow_mask_H, out=np.ones_like(V_supply_d_t, dtype=float))
+          where=overflow_mask_H_d_t, out=np.ones_like(V_supply_d_t, dtype=float))
         ratios_C = np.divide(
           np.full(len(V_supply_d_t), V_hs_dsgn_C, dtype=float),
           np.ceil(V_supply_d_t * 1000) / 1000,
-          where=overflow_mask_C, out=np.ones_like(V_supply_d_t, dtype=float))
+          where=overflow_mask_C_d_t, out=np.ones_like(V_supply_d_t, dtype=float))
 
         new_V_supply_d_t_i = V_supply_d_t_i * ratios_H[np.newaxis, :] * ratios_C[np.newaxis, :]
 
+        """ 事後条件を確認 """
         check = np.sum(new_V_supply_d_t_i, axis=0)
         assert all(check[H] <= V_hs_dsgn_H)
         assert all(check[C] <= V_hs_dsgn_C)
 
     elif constants.change_V_supply_d_t_i_max == Vサプライの上限キャップ.ピンポイントでキャップ.value:
-        # TODO: 実装する
-        new_V_supply_d_t_i = np.clip(V_supply_d_t_i, V_vent_g_i, V_dash_supply_d_t_i)
+        # デンソーエアクール 守屋さまより提案 案2
+
+        """ 設計風量をキャップ上限とする """
+        V_hs_dsgn_C = V_hs_dsgn_C if V_hs_dsgn_C is not None else float('inf')
+        V_hs_dsgn_H = V_hs_dsgn_H if V_hs_dsgn_H is not None else float('inf')
+
+        """ キャップを超える時刻を調べる """
+        V_supply_d_t_i = np.clip(V_supply_d_t_i, V_vent_g_i, None)
+        V_supply_d_t = np.sum(V_supply_d_t_i, axis=0)  # 1d-shape(5, )
+
+        overflow_mask_H_d_t = np.logical_and(H, V_supply_d_t > V_hs_dsgn_H)
+        overflow_mask_C_d_t = np.logical_and(C, V_supply_d_t > V_hs_dsgn_C)
+
+        # 二次元と見なして使用
+        overflow_mask_H_d_t_i = np.tile(overflow_mask_H_d_t, (5,1))
+        overflow_mask_C_d_t_i = np.tile(overflow_mask_C_d_t, (5,1))
+
+        """ 縮小対象のセルを調査 """  # 個別適用なので案2では2d-array
+        added_mask_d_t_i = V_supply_d_t_i > V_dash_supply_d_t_i
+
+        target_mask_H_d_t_i = np.logical_and(added_mask_d_t_i, overflow_mask_H_d_t_i)
+        target_mask_C_d_t_i = np.logical_and(added_mask_d_t_i, overflow_mask_C_d_t_i)
+
+        """ 縮小対象セルの削減量を計算 """
+        # 全体で削減すべき量
+        overflow_values_H_d_t = V_supply_d_t - V_hs_dsgn_H
+        overflow_values_C_d_t = V_supply_d_t - V_hs_dsgn_C
+        # 削減場所では限界値以上になっている
+
+        masked_vs_H_d_t_i = np.where(target_mask_H_d_t_i, V_supply_d_t_i, 0)
+        added_sums_H_d_t = np.sum(masked_vs_H_d_t_i, axis=0)
+        added_sums_H_d_t_i = np.tile(added_sums_H_d_t, (5,1))
+
+        masked_vs_C_d_t_i = np.where(target_mask_C_d_t_i, V_supply_d_t_i, 0)
+        added_sums_C_d_t = np.sum(masked_vs_C_d_t_i, axis=0)
+        added_sums_C_d_t_i = np.tile(added_sums_C_d_t, (5,1))
+
+        default_subtract_d_t_i = np.zeros_like(V_supply_d_t_i)
+
+        ratio_H_d_t_i = np.divide(
+            masked_vs_H_d_t_i,
+            np.floor(added_sums_H_d_t_i * 1000) / 1000,  # 超えない工夫(引くのを大き目に)
+            where=target_mask_H_d_t_i, out=default_subtract_d_t_i)
+        # 削減量に値の割合を適用
+        subtract_H_d_t_i = ratio_H_d_t_i * np.tile(overflow_values_H_d_t, (5,1))
+
+        ratio_C_d_t_i = np.divide(
+            masked_vs_C_d_t_i,
+            np.floor(added_sums_C_d_t_i * 1000) / 1000,
+            where=target_mask_C_d_t_i, out=default_subtract_d_t_i)
+        # 削減量に値の割合を適用
+        subtract_C_d_t_i = ratio_C_d_t_i * np.tile(overflow_values_C_d_t, (5,1))
+
+        """ 元から制限を超えてしまってないか念のためチェックします """
+        added_mask_d_t = np.sum(added_mask_d_t_i, axis=0)
+        # NOTE: 増加していないのに、制限を超えてしまっている時刻がないか
+        should_be_target = np.logical_or(overflow_values_H_d_t > 0, overflow_values_C_d_t > 0)
+        errors = np.logical_and(added_mask_d_t == 0, should_be_target)
+        assert not np.any(errors), "元から制限を超えている時刻があるようです."
+
+        """ 減算の実行 """
+        new_V_supply_d_t_i = np.where(
+            target_mask_H_d_t_i,  # 引き算対象セル
+            V_supply_d_t_i - subtract_H_d_t_i,
+            V_supply_d_t_i)  # 引き算しない箇所の値
+
+        new_V_supply_d_t_i = np.where(
+            target_mask_C_d_t_i,  # 引き算対象セル
+            new_V_supply_d_t_i - subtract_C_d_t_i,
+            new_V_supply_d_t_i)  # 引き算しない箇所の値
+
+        """ 事後条件の確認"""
+        check = np.sum(new_V_supply_d_t_i, axis=0)
+        # TODO: バグ修正して有効にする
+        assert all(check[H] <= V_hs_dsgn_H)
+        assert all(check[C] <= V_hs_dsgn_C)
 
     else:
         raise ValueError("change_V_supply_d_t_i is out of range")
